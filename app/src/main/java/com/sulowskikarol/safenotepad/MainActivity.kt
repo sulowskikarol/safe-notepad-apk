913d3191f39801b4cdd45a68c68fbf59657284df
sulowskikarol <karolsu128@gmail.com>
2026-04-09 16:29:22 +0200

    main activity UI

--- app/src/main/java/com/sulowskikarol/safenotepad/MainActivity.kt
+++ app/src/main/java/com/sulowskikarol/safenotepad/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.sulowskikarol.safenotepad
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    enableEdgeToEdge()
-    setContentView(R.layout.activity_main)
-    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-      val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-      insets
+
+    private lateinit var noteEditText: TextInputEditText
+    private lateinit var saveButton: Button
+    private lateinit var loadButton: Button
+    private lateinit var clearButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Inicjalizacja widoków
+        noteEditText = findViewById(R.id.noteEditText)
+        saveButton = findViewById(R.id.saveButton)
+        loadButton = findViewById(R.id.loadButton)
+        clearButton = findViewById(R.id.clearButton)
+
+        // Obsługa kliknięć
+        saveButton.setOnClickListener {
+            // TODO: Zapisz notatkę
+        }
+
+        loadButton.setOnClickListener {
+            // TODO: Wczytaj notatkę
+        }
+
+        clearButton.setOnClickListener {
+            // TODO: Wyczyść dane
+        }
     }
-  }
 }
